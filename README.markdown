6c5a032926f89c06eea0a4a61f4939bdd53df732
James Halliday <mail@substack.net>
2011-11-10 00:22:21 -0800

    updated the readme

--- README.markdown
+++ README.markdown
@@ -13,7 +13,7 @@ var fs = require('fs');
 var out = gutter({
     name : 'words',
     version : '0.2.1',
-    words : fs.createReadStream('/usr/share/dict/words')
+    words : fs.createReadStream('/usr/share/dict/words', { encoding : 'utf8' })
 });
 
 out.pipe(process.stdout);
--- example/words.js
+++ example/words.js
@@ -1,14 +1,10 @@
 var gutter = require('../');
 var fs = require('fs');
-var es = require('event-stream');
 
 var out = gutter({
     name : 'words',
     version : '0.2.1',
-    words : es.connect(
-        fs.createReadStream('/usr/share/dict/words'),
-        es.split()
-    )
+    words : fs.createReadStream('/usr/share/dict/words', { encoding : 'utf8' })
 });
 
 out.pipe(process.stdout);
