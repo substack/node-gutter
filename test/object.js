774100deda6e7b2a4963d42a9da30231f1e698a2
James Halliday <mail@substack.net>
2012-02-22 09:13:19 -0800

    stream test checks streams and emitters now

--- test/object.js
+++ test/object.js
@@ -1,24 +1,46 @@
 var gutter = require('../');
 var test = require('tap').test;
+var Stream = require('stream').Stream;
 
 var EventEmitter = require('events').EventEmitter;
 
-test('object emitter', function (t) {
+test('object emitter and stream', function (t) {
     t.plan(1);
     
     var ev = new EventEmitter;
     ev.type = 'object';
-    var i = 0;
-    var iv = setInterval(function () {
-        if (++i === 5) {
-            clearInterval(iv);
-            ev.emit('end');
-        }
-        else ev.emit('data', [ String.fromCharCode(i + 96), i ])
-    }, 10);
+    (function () {
+        var i = 0;
+        var iv = setInterval(function () {
+            if (++i === 5) {
+                clearInterval(iv);
+                ev.emit('end');
+            }
+            else ev.emit('data', [ String.fromCharCode(i + 96), i ])
+        }, 10);
+    })();
+    
+    var stream = new Stream;
+    stream.type = 'object';
+    (function () {
+        var i = 0;
+        var iv = setInterval(function () {
+            if (++i === 4) {
+                clearInterval(iv);
+                stream.emit('end');
+            }
+            else stream.emit('data', [
+                String.fromCharCode(97 + 26 - i),
+                i * 100
+            ])
+        }, 10);
+    })();
     
     var s = gutter([
-        'foo', 5, 'bar', { baz : ev }
+        { foo : stream },
+        'bar',
+        11,
+        { baz : ev }
     ]);
     
     var data = '';
@@ -29,7 +51,12 @@ test('object emitter', function (t) {
     s.on('end', function () {
         t.deepEqual(
             JSON.parse(data),
-            [ 'foo', 5, 'bar', { baz : { a : 1, b : 2, c : 3, d : 4 } } ]
+            [
+                { foo : { z : 100, y : 200, x : 300 } },
+                'bar',
+                11,
+                { baz : { a : 1, b : 2, c : 3, d : 4 } },
+            ]
         );
         t.end();
     });
