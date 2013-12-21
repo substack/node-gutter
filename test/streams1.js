f44b5df7f530041b60caec27bd5df1ebff349d2e
James Halliday <mail@substack.net>
2013-12-21 00:04:55 -0800

    failing streams1 test

---
+++ test/streams1.js
@@ -0,0 +1,33 @@
+var test = require('tape');
+var through = require('through');
+var concat = require('concat-stream');
+var gutter = require('../');
+
+test('expand a streams1 stream', function (t) {
+    t.plan(1);
+    
+    var stream = through();
+    stream.pause();
+    stream.queue('a');
+    stream.queue('b');
+    stream.queue('c');
+    
+    var out = gutter({
+        name : 'greetings',
+        version : [1,2,3],
+        greetings : stream,
+        beep : 'boop'
+    });
+    out.pipe(concat(function (body) {
+        t.deepEqual(JSON.parse(body), {
+            name: 'greetings',
+            version: [1,2,3],
+            greetings: ['abc','d'],
+            beep: 'boop'
+        });
+    }));
+
+    stream.resume();
+    stream.queue('d');
+    stream.queue(null);
+});
