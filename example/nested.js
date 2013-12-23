d2f2b30ef179a573df112f70704f162a2d002692
James Halliday <mail@substack.net>
2013-12-22 18:52:15 -0800

    failing nested test and example

---
+++ example/nested.js
@@ -0,0 +1,21 @@
+var through2 = require('through2');
+var concat = require('concat-stream');
+var gutter = require('../');
+
+var b = through2();
+b.push('9');
+b.push('8!');
+b.push(null);
+
+var stream = through2({ objectMode: true });
+
+stream.push({ a: 97 });
+stream.push({ b: b });
+stream.push({ c: 99 });
+
+var out = gutter({
+    before : 'AAA',
+    stream: stream,
+    after : 'ZZZ'
+});
+out.pipe(process.stdout);
---
+++ test/nested.js
@@ -0,0 +1,32 @@
+var test = require('tape');
+var through2 = require('through2');
+var concat = require('concat-stream');
+var gutter = require('../');
+
+test('stream in a stream', function (t) {
+    t.plan(1);
+    
+    var b = through2();
+    b.push('9');
+    b.push('8!');
+    b.push(null);
+    
+    var stream = through2({ objectMode: true });
+    
+    stream.push({ a: 97 });
+    stream.push({ b: b });
+    stream.push({ c: 99 });
+    
+    var out = gutter({
+        before : 'AAA',
+        stream: stream,
+        after : 'ZZZ'
+    });
+    out.pipe(concat(function (body) {
+        t.deepEqual(JSON.parse(body), {
+            before: 'AAA',
+            stream: [ { a: 97 }, { b: [ '98!' ] }, { c: 99 } ],
+            after: 'ZZZ'
+        });
+    }));
+});
